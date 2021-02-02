11e7786245549d23a862257b8c43adee961e3c1b
João Pedro Salgado <joao.salgado@enerwatt.com.br>
2021-02-01 21:51:40 -0300

    Adicionando Projeto do CreciSP

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CreciSP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(505, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(644, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Gravar na Planilha";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(762, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Página Inicial";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(4, 50);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1237, 462);
+            this.webBrowser1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(570, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 21);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Navegar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(974, -8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 61);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(861, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 20);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Recomeçar Processo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 510);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Creci-SP ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,425 @@
+﻿
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace CreciSP
+{
+    public partial class Form1 : Form
+    {
+
+
+        List<string> numeroRegistro = new List<string>();
+
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+
+        private string _applicationName = "Lista de Corretores - CRECISP";
+
+        private string _spreadsheetId = "1KUWSn5Mr8AzqvL3-TBVilIDioTV7AKxDZ2dZXm4nJxw";
+
+        private SheetsService _sheetsService;
+
+        static readonly string sheet = "Lista Corretores";
+
+        private int i = 0;
+
+        private int quantidadeCorretores = 0;
+
+        private string urlAtual = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void inserirNaPlanilha(List<IList<object>> data, object sender, EventArgs e, int i)
+        {
+            try
+            {
+                if (webBrowser1.ReadyState == WebBrowserReadyState.Complete)
+                {
+
+                    Google.Apis.Auth.OAuth2.GoogleCredential credential;
+                    // Put your credentials json file in the root of the solution and make sure copy to output dir property is set to always copy 
+
+                    using (var stream = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "credentials.json", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                    {
+
+                        credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+
+                    }
+
+                    // Create Google Sheets API service.
+                    _sheetsService = new SheetsService(new BaseClientService.Initializer()
+                    {
+                        HttpClientInitializer = credential,
+                        ApplicationName = _applicationName
+                    });
+
+                    var range = $"{sheet}!A:E";
+                    var valueRange = new ValueRange();
+                    valueRange.Values = data;
+
+                    var appendRequest = _sheetsService.Spreadsheets.Values.Append(valueRange, _spreadsheetId, range);
+                    appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
+                    var appendReponse = appendRequest.Execute();
+
+                    var html = webBrowser1.Document.Body.InnerHtml;
+                    var url = webBrowser1.Url.AbsoluteUri;
+                    url = webBrowser1.Url.AbsoluteUri;
+
+                    webBrowser1.AllowNavigation = true;
+
+
+                    webBrowser1.DocumentCompleted -= ProcessoCorretores;
+                    this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(this.button1_Click);
+                    this.webBrowser1.Navigate(urlAtual);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void ProcessoCorretores(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            List<IList<object>> dadosCorretor = new List<IList<object>>();
+            IList<object> DadosCorretor = new List<object>();
+
+            if ((sender as WebBrowser).ReadyState == System.Windows.Forms.WebBrowserReadyState.Complete)
+            {
+
+
+
+                // Do what ever you want to do here when page is completely loaded.
+
+                HtmlElementCollection getNomeCorretor = webBrowser1.Document.GetElementsByTagName("h3");
+
+
+
+
+
+                var url = webBrowser1.Url.AbsoluteUri;
+
+                foreach (HtmlElement elem in getNomeCorretor)
+                {
+                    //Nome
+                    if (elem.InnerHtml != "Telefones" && elem.InnerHtml != null && elem.InnerHtml.Length > 2)
+                    {
+
+                        DadosCorretor.Add(elem.InnerHtml);
+                    }
+                }
+
+
+                HtmlElementCollection getDadosRestantes = webBrowser1.Document.GetElementsByTagName("span");
+
+                foreach (HtmlElement elem in getDadosRestantes)
+                {
+                    //Nome
+                    if (elem != null && elem.InnerHtml != "" && elem.InnerHtml != null && elem.Parent.InnerHtml != ""
+                        && elem.Parent.InnerHtml != null
+                        && (elem.Parent.InnerHtml.Contains("CRECI") ||
+                        elem.Parent.InnerHtml.Contains("Situação") ||
+                        elem.Parent.InnerHtml.Contains("E-Mail Oficial") ||
+                        elem.Parent.InnerHtml.Contains("\n                                            <span>")) &&
+                        !elem.InnerHtml.Contains("<i class=\"fa fa-spinner fa-spin \"></i>"))
+                    {
+
+                        if (DadosCorretor.Contains(" Ativo") && (elem.InnerHtml.Length == 15 || elem.InnerHtml.Length == 16 || elem.InnerHtml.Length == 14))
+                        {
+                            if (DadosCorretor.Count == 4 || DadosCorretor.Count == 5 || DadosCorretor.Count == 6)
+                            {
+                                DadosCorretor.Add(elem.InnerHtml);
+                            }
+                            else
+                            {
+                                if (DadosCorretor.Contains("("))
+                                {
+                                    DadosCorretor.Add(elem.InnerHtml);
+                                }
+                                else
+                                {
+                                    DadosCorretor.Add("");
+                                    DadosCorretor.Add(elem.InnerHtml);
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            DadosCorretor.Add(elem.InnerHtml);
+                        }
+
+
+
+                    }
+                }
+
+                dadosCorretor.Add(DadosCorretor);
+
+
+                inserirNaPlanilha(dadosCorretor, sender, e, i);
+
+
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (webBrowser1.Document != null)
+            {
+                quantidadeCorretores = webBrowser1.Document.GetElementsByTagName("h6").Count;
+
+                if (quantidadeCorretores > numeroRegistro.Count)
+                {
+
+
+                    if (webBrowser1.ReadyState == System.Windows.Forms.WebBrowserReadyState.Complete)
+                    {
+                        this.webBrowser1.DocumentCompleted -= button1_Click;
+                        this.webBrowser1.DocumentCompleted += ProcessoCorretores;
+
+                        HtmlElementCollection elems = webBrowser1.Document.GetElementsByTagName("span");
+                        var url = webBrowser1.Url.AbsoluteUri;
+
+                        if (webBrowser1.Url.AbsoluteUri == "https://www.crecisp.gov.br/cidadao/listadecorretores")
+                        {
+                            urlAtual = "https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i;
+                            url = "https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i;
+                        }
+
+                        foreach (HtmlElement elem in elems)
+                        {
+                            if (!numeroRegistro.Contains(elem.InnerHtml) && quantidadeCorretores > numeroRegistro.Count)
+                            {
+                                if (elem.InnerHtml != null && elem.InnerHtml != "" && elem.InnerHtml.Length == 9)
+                                {
+                                    if (!numeroRegistro.Contains(elem.InnerHtml) && quantidadeCorretores > numeroRegistro.Count)
+                                    {
+
+                                        numeroRegistro.Add(elem.InnerHtml);
+
+                                        HtmlElementCollection buttonClick = elem.Parent.Parent.GetElementsByTagName("button");
+
+                                        foreach (HtmlElement elemt in buttonClick)
+                                        {
+                                            if (elemt.InnerHtml == " Ver Detalhes ")
+                                            {
+                                                elemt.InvokeMember("click");
+                                                break;
+                                            }
+                                        }
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        if (webBrowser1.Document.Body.GetAttribute("class") == "navigate")
+                                        {
+                                            i++;
+                                            string[] getPage = null;
+                                            url = webBrowser1.Url.AbsoluteUri;
+                                            if (url.Contains("firstLetter="))
+                                            {
+                                                getPage = url.Split('=');
+
+                                                var first = char.Parse(getPage[2]);
+
+                                                numeroRegistro = new List<string>();
+                                                webBrowser1.AllowNavigation = true;
+                                                this.webBrowser1.Navigate("https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i + "&firstLetter=" + first);
+                                                urlAtual = "https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i + "&firstLetter=" + first;
+                                            }
+                                            else
+                                            {
+                                                numeroRegistro = new List<string>();
+                                                webBrowser1.AllowNavigation = true;
+                                                this.webBrowser1.Navigate("https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i);
+                                                urlAtual = "https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i;
+                                            }
+
+                                        }
+                                        else
+                                        {
+                                            url = webBrowser1.Url.AbsoluteUri;
+                                            string[] getPage = null;
+                                            char first = ' ';
+                                            char nextChar = ' ';
+                                            i = 0;
+                                            if (url.Contains("firstLetter="))
+                                            {
+                                                getPage = url.Split('=');
+
+
+                                                if (getPage != null && getPage.Length == 2)
+                                                {
+                                                    first = char.Parse(getPage[1]);
+                                                    nextChar = (char)((char)first + 1);
+                                                }
+
+                                                if (getPage != null && getPage.Length == 3)
+                                                {
+                                                    first = char.Parse(getPage[2]);
+                                                    nextChar = (char)((char)first + 1);
+                                                }
+
+                                                numeroRegistro = new List<string>();
+                                                webBrowser1.AllowNavigation = true;
+                                                this.webBrowser1.Navigate("https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i + "&firstLetter=" + nextChar);
+                                                urlAtual = "https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i + "&firstLetter=" + nextChar;
+                                            }
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+
+                    if (webBrowser1.Document.Body.InnerHtml.Contains("navigate"))
+                    {
+                        i++;
+                        string[] getPage = null;
+                        var url = webBrowser1.Url.AbsoluteUri;
+                        if (url.Contains("firstLetter="))
+                        {
+                            getPage = url.Split('=');
+
+                            var first = char.Parse(getPage[2]);
+
+                            numeroRegistro = new List<string>();
+                            webBrowser1.AllowNavigation = true;
+                            this.webBrowser1.Navigate("https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i + "&firstLetter=" + first);
+                            urlAtual = "https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i + "&firstLetter=" + first;
+                        }
+                        else
+                        {
+                            numeroRegistro = new List<string>();
+                            webBrowser1.AllowNavigation = true;
+                            this.webBrowser1.Navigate("https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i);
+                            urlAtual = "https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i;
+                        }
+
+
+                    }
+                    else
+                    {
+                        var url = webBrowser1.Url.AbsoluteUri;
+                        string[] getPage = null;
+                        char first = ' ';
+                        char nextChar = ' ';
+                        i = 0;
+                        if (url.Contains("firstLetter="))
+                        {
+                            getPage = url.Split('=');
+
+
+                            if (getPage != null && getPage.Length == 2)
+                            {
+                                first = char.Parse(getPage[1]);
+                                nextChar = (char)((char)first + 1);
+                            }
+
+                            if (getPage != null && getPage.Length == 3)
+                            {
+                                first = char.Parse(getPage[2]);
+                                nextChar = (char)((char)first + 1);
+                            }
+
+                            numeroRegistro = new List<string>();
+                            webBrowser1.AllowNavigation = true;
+                            this.webBrowser1.Navigate("https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i + "&firstLetter=" + nextChar);
+                            urlAtual = "https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i + "&firstLetter=" + nextChar;
+                        }
+                    }
+                }
+
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void Voltar_Click(object sender, EventArgs e)
+        {
+            (sender as WebBrowser).GoBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Navigate("https://www.crecisp.gov.br/cidadao/buscaporcorretores");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var url = webBrowser1.Url.AbsoluteUri;
+            var getPage = url.Split('=');
+
+            webBrowser1.AllowNavigation = true;
+
+
+            webBrowser1.DocumentCompleted -= button4_Click;
+            this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(this.button1_Click);
+
+
+            if (getPage.Length == 3)
+            {
+                var pagina = getPage[1].Split('&');
+                i = int.Parse(pagina[0]);
+                this.webBrowser1.Navigate(url);
+                urlAtual = url;
+            }
+
+            if (getPage.Length == 2)
+            {
+                //i = int.Parse(getPage[1]);
+                int inteiro;
+                var verificacao = Int32.TryParse(getPage[1], out inteiro);
+
+                if (verificacao == false)
+                {
+                    this.webBrowser1.Navigate(url);
+                    urlAtual = url;
+                }
+                else
+                {
+                    i = inteiro;
+                    this.webBrowser1.Navigate("https://www.crecisp.gov.br/cidadao/listadecorretores?page=" + i);
+                    urlAtual = url;
+                }
+            }
+
+        }
+    }
+}
